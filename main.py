6e4d39522a9e4d4884937323fadf548cf93942aa
Rolly Maulana Awangga <rolly@awang.ga>
2015-11-11 18:24:19 +0700

    display status

--- main.py
+++ main.py
@@ -14,8 +14,13 @@ b = open(sys.argv[2],'w')
 writer = csv.writer(b)
 print "Mulai.. sabar mang......"
 writer.writerow(table.field_names)
+i=0
 for record in table:
 	lst=list(record.values())
 	writer.writerow(lst)
+	i=i+1
+	#print i
+	print '{record diproses}\r',
+	print i,
 b.close()
 
